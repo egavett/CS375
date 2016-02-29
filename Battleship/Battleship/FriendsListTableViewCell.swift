ba213b9d3b8446b1271452abb7723353485137bb
egavett <egavett35@gmail.com>
2016-02-28 16:04:14 -0800

    Day 1
    - Created storyboard
    - Created base classes

---
+++ Battleship/Battleship/FriendsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsListTableViewCell.swift
+//  Battleship
+//
+//  Created by Elijah L Gavett on 2/28/16.
+//  Copyright © 2016 Elijah L Gavett. All rights reserved.
+//
+
+import UIKit
+
+class FriendsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Battleship/Battleship/FriendsListTableViewController.swift
@@ -0,0 +1,122 @@
+//
+//  FriendsListTableViewController.swift
+//  Battleship
+//
+//  Created by Elijah L Gavett on 2/28/16.
+//  Copyright © 2016 Elijah L Gavett. All rights reserved.
+//
+
+import UIKit
+
+class FriendsListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let toolbar = UIToolbar()
+        toolbar.frame = CGRectMake(0, self.view.frame.size.height - 46, self.view.frame.size.width, 46)
+        //toolbar.sizeToFit()
+        
+        let item1 = UIBarButtonItem(title: "Add", style: .Plain, target: self, action: "addFriend")
+        let item2 = UIBarButtonItem(title: "Remove", style: .Plain, target: self, action: "removeFriend")
+        
+        toolbar.setItems([item1, item2], animated: true)
+        
+        //toolbar.setItems(toolbarButtons, animated: true)
+        toolbar.backgroundColor = UIColor.redColor()
+        self.view.addSubview(toolbar)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    func addFriend(sender: AnyObject) {
+        print("You added a new friend!")
+        // TODO - push controller to add friend
+    }
+    
+    func removeFriend(sender: AnyObject) {
+        print("You just lost a friend...")
+        // TODO - edit table to remove a friend
+    }
+
+    
+    
+    
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
